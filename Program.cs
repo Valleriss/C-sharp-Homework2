619f866dd40e8e7999eb5568f853a764eb32035b
LeraLukashova <lerashilova07@gmail.com>
2023-04-02 19:37:00 +0300

    исправленное решение task10

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 /*
 Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
